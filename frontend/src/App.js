f75257c74c7412209326a24d3e610fccfbd04689
Eze1998 <111812412+Eze1998@users.noreply.github.com>
2023-01-14 00:04:53 -0300

    css y app.js avanzado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Album from './Componentes/index'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor'>
+        <nav>
+          <h1>Figuritas</h1>
+        </nav>
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Album/>}/>
+        </Routes>
+        </BrowserRouter>
+  
+      </div>
     </div>
   );
 }
