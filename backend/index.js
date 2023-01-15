995007e8a143e9f3e27ac4b5c5f1591d716fe603
Eze1998 <111812412+Eze1998@users.noreply.github.com>
2023-01-15 20:09:49 -0300

    mas back

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const Sequelize = require('sequelize')
+const Model = require('sequelize')
+const DataTypes = require('sequelize')
+
+const express = require('express');
+const figuritaRoutes = require('./Controlador/figuritas');
+const app = express();
+app.use(express.json());
+app.use('/',figuritaRoutes);
+app.listen(8080,()=>{
+    console.log('I\'m working')
+})
--- hola.js
+++
@@ -1,2 +0,0 @@
-kfkffd
-caches;vclkvc
