c530e20fa518154b6942febeaa899892de3f3a8a
Eze1998 <111812412+Eze1998@users.noreply.github.com>
2023-01-14 23:24:29 -0300

    back avanzado

---
+++ backend/Modelos/figurita.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, Sequelize) => {
+    const Figurita = sequelize.define("Figurita", {
+      id: {
+        type: Sequelize.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nombre: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pais: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      tengo: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        defaultValue :'0'
+      }
+      },{
+          tableName : "figurita"
+      });
+    return Figurita;
+  };
+  
